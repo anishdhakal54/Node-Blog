52e1b22d398d57441ba9fa8238e8091b5a4f611a
anishdhakal54 <anish.dhakal54@gmail.com>
2019-11-26 09:41:50 +0545

    added express edge template and create post form

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-const http = require("http");
+const path = require("path");
+const { config, engine } = require("express-edge");
+const express = require("express");
+const mongoose = require("mongoose");
 
-const server = http.createServer((req, res) => {
-  res.end("Hello from the server");
+const app = express();
+
+mongoose.connect("mongodb://localhost/nodeblog");
+
+app.use(express.static("public"));
+app.use(engine);
+
+app.set("views", `${__dirname}/views`);
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-server.listen(3000, () => {
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+
+app.get("/post/new", (req, res) => {
+  res.render("create");
+});
+
+app.listen(3000, () => {
   console.log("Server is running");
 });
