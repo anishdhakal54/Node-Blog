bd98bc513d32778e05923860b9322dd459d557fc
anishdhakal54 <anish.dhakal54@gmail.com>
2019-11-30 00:53:50 -0800

    Create post using database and model

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ const path = require("path");
 const { config, engine } = require("express-edge");
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const Post = require("./database/models/Post");
 
 const app = express();
 
-mongoose.connect("mongodb://localhost/nodeblog");
+mongoose.connect("mongodb://localhost/nodeblog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(express.static("public"));
 app.use(engine);
 
 app.set("views", `${__dirname}/views`);
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -30,6 +39,12 @@ app.get("/post", (req, res) => {
 
 app.get("/post/new", (req, res) => {
   res.render("create");
+});
+
+app.post("/post/store", (req, res) => {
+  Post.create(req.body, (err, post) => {
+    res.redirect("/");
+  });
 });
 
 app.listen(3000, () => {
